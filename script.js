60c0cd90c16f6184da02dbdaaca676ffa60736f2
MikeLuxuryPalmeras <mike@luxurypalmeras.com>
2024-03-24 16:51:42 -0400

    fixed form

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 function validateForm(){
     const forms = document.getElementById('recensieForm')
-  
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
+        console.log(form)
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
         }
+        console.log("pass")
         form.classList.add('was-validated')
       }, false)
     })
